43dc778110a5dc8f8a325754ac78da9e209e27d7
Khalil <khalil.hersi@hotmail.co.uk>
2022-11-10 23:17:43 +0000

    adds guess integer and game is now functional.

--- ControlFlow/src/main/java/Main.java
+++ ControlFlow/src/main/java/Main.java
@@ -1,29 +1,27 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        int secretNumber = 5; //the secret number
+        System.out.println("Choose a number between 1-20."); //gives the following message
+        Scanner reader = new Scanner(System.in); //allows for input to be made
+         int guess = reader.nextInt(); //guess is the number the user types
 
-        Scanner reader = new Scanner(System.in);
-        String guess = reader.nextLine();
 
-        int number = 5;
-
-
-        //boolean isCorrect = false;
-
-            if(guess < 5){
-                System.out.println("The secret number is higher than your chosen number");;
-            }
-            else if(guess > 5){
+        if(secretNumber > guess){
+                System.out.println("The secret number is higher than your chosen number");
+            } //if input number is less than secret number, this message is shown.
+            else if(secretNumber < guess){
                 System.out.println("The secret number is lower than your chosen number");
-            }
-            else {
+            }//if input number is greater than secret number, this message is shown.
+            else if (secretNumber == secretNumber){
                 System.out.println("You have figured out the secret number, 5! Well done!");
-                //isCorrect = true;git
-            }
+            }//User entered the correct number.
+        }
 
 
 
 
     }
-}
+
