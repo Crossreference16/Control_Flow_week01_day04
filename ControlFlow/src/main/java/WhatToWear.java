71892a9ad02c20fe5d3a609d3629f8e91603d199
Khalil <khalil.hersi@hotmail.co.uk>
2022-11-10 23:30:10 +0000

    adds a new java class for what clothing to wear during various weather conditions

---
+++ ControlFlow/src/main/java/WhatToWear.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class WhatToWear {
+    public static void main(String[] args) {
+        System.out.println("Wondering what to wear today?");
+
+
+        Scanner reader = new Scanner(System.in);
+
+        String weather = reader.nextLine();
+        String clothing = reader.nextLine();
+
+
+        System.out.println();
+
+
+
+
+    }
+}
