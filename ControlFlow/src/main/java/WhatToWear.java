84a1def7b9242ee724eb0fd27c22fa334f6e9a1e
Khalil <khalil.hersi@hotmail.co.uk>
2022-11-11 00:20:51 +0000

    adds functionality to clothing app. Able to distinguish between weather and temp, will let you know what is best to wear

--- ControlFlow/src/main/java/WhatToWear.java
+++ ControlFlow/src/main/java/WhatToWear.java
@@ -2,16 +2,49 @@ import java.util.Scanner;
 
 public class WhatToWear {
     public static void main(String[] args) {
-        System.out.println("Wondering what to wear today?");
+        System.out.println("Wondering what to wear today? What is the weather like?");
+        System.out.println("Cloudy, rainy, sunny or snowy?");
 
 
         Scanner reader = new Scanner(System.in);
 
         String weather = reader.nextLine();
-        String clothing = reader.nextLine();
+        System.out.println("what is the temperature?");
+        int temp = reader.nextInt();
 
 
-        System.out.println();
+
+
+
+        if (weather.equals("rainy")) {
+            if (temp < 12) {
+                System.out.println("Wear a waterproof winter coat, its cold and wet!");
+            }
+            else {
+                System.out.println("Wet but not cold. An umbrella should suffice!");
+            }
+        }
+        else if(weather.equals(("cloudy"))){
+            if (temp < 14){System.out.println("Wear a coat, theres no sun to warm you!");}
+            else {
+                System.out.println("No coat needed, but I wish it was sunny :(");
+            }
+        }
+        else if(weather.equals(("sunny"))){
+            if (temp < 13){System.out.println("Sunny AND cold? Bummer. Time to get your coat.");}
+            else if (temp <= 16){
+                System.out.println("No coat needed, its a nice day!");
+            }
+            else {
+                System.out.println("T-shirt weather, amazing! Wear your best colours!");
+            }
+        }
+        else if (weather.equals("snow")) {
+                System.out.println("Wear gloves, a hat and a THICK coat. BRRR!");
+            }
+
+
+
 
 
 
